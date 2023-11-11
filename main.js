1f64ff04cbe178bd38e918fdb00685db0fb080bd
AliMousazadeh <ali.mousazadeh98@gmail.com>
2023-11-11 21:29:44 +0100

    Better version.

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 import { readCSV, findRows } from './myUtils.js';
 
-
 const [filePath, columnNumber, query] = process.argv.slice(2);
 console.log(filePath, columnNumber, query);
 
-findRows(filePath, columnNumber, query);
+
+readCSV(filePath)
+  .then((csvFile) => findRows(csvFile, columnNumber, query))
+  .then((rows) => {
+    console.log(rows);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
--- myUtils.js
+++ myUtils.js
@@ -3,16 +3,16 @@ import csv from 'csv-parser';
 
 
 export const readCSV = function (filePath) {
-  const results = [];
+  const csvFile = [];
 
   return new Promise((resolve, reject) => {
     createReadStream(filePath)
       .pipe(csv({ headers: false }))
       .on('data', (data) => {
-        results.push(data);
+        csvFile.push(data);
       })
       .on('end', () => {
-        resolve(results);
+        resolve(csvFile);
       })
       .on('error', (error) => {
         reject(error);
@@ -21,21 +21,12 @@ export const readCSV = function (filePath) {
 }
 
 
-export const findRows = function (filePath, columnNumber, query) {
-  readCSV(filePath)
-    .then((data) => {
-      const selectedColumn = data.map(row => row[columnNumber]);
-      const indices = selectedColumn.reduce((accumulator, current, index) => {
-        if (current === query) {
-          accumulator.push(index);
-        }
-        return accumulator;
-      }, []);
+export const findRows = function (csvFile, columnNumber, query) {
+  const selectedColumn = csvFile.map(row => row[columnNumber]);
 
-      const results = indices.map(indices => Object.values(data[indices]));
-      console.log(results);
-    })
-    .catch((error) => {
-      console.error('Error reading CSV:', error);
-    });
-}
+  const indices = selectedColumn.reduce((accumulator, current, index) => {
+    return current === query? accumulator.concat(index) : accumulator;
+  }, []);
+
+  return indices.map(indices => Object.values(csvFile[indices]));
+};
